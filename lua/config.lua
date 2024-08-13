996e49f222aefc2ffba36cc66c4a7c6fd98f30e6
Theodore A. Roth <troth@openavr.org>
2024-08-13 14:52:43 -0600

    Initial commit
    Signed-off-by: Theodore A. Roth <troth@openavr.org>

---
+++ lua/config.lua
@@ -0,0 +1,12 @@
+-- General configuration options
+vim.opt.compatible = false
+
+-- Mouse Support
+vim.opt.mouse = "v"
+vim.opt.mouse = "a"
+
+-- Tabs
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.tabstop = 4
