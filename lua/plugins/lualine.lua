2874f482ba2b1da7bb49032dc06abfd0b2c3c883
Theodore A. Roth <troth@openavr.org>
2026-03-19 17:07:24 -0600

    lualine: Show relative path to file in buffer
    Signed-off-by: Theodore A. Roth <troth@openavr.org>

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,6 +4,9 @@ return {
         require('lualine').setup({
             options = {
                 theme = 'Tomorrow'
+            },
+            sections = {
+                lualine_c = { { 'filename', path=1 } }
             }
         })
     end
