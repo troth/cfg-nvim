3c81eea07978abd820c562c449d6407f5c3d9080
Theodore A. Roth <troth@openavr.org>
2026-03-20 14:54:17 -0600

    Update telescope config
    Signed-off-by: Theodore A. Roth <troth@openavr.org>

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,16 +1,22 @@
 return {
     {
         'nvim-telescope/telescope.nvim',
-        tag = '0.1.8',
+        tag = 'v0.2.1',
         -- or
         -- branch = '0.1.x',
-        dependencies = { 'nvim-lua/plenary.nvim' },
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            -- optional but recommended
+            { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+        },
+
         config = function()
             local builtin = require("telescope.builtin")
             vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
             vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
             vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-            vim.keymap.set('n', '<leader>fs', builtin.grep_string, {})
+            vim.keymap.set('n', '<leader>fw', builtin.grep_string, {
+                desc = 'Find word under cursor' })
         end
     },
     {
