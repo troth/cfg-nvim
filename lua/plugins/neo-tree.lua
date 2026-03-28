327a22b216d684e4080f868fc3aea8f924c94b01
Theodore A. Roth <troth@openavr.org>
2026-03-28 06:06:34 -0600

    neo-tree: Uses opts instead of config = function()
    Signed-off-by: Theodore A. Roth <troth@openavr.org>

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -6,30 +6,32 @@ return {
         "nvim-tree/nvim-web-devicons",
         "MunifTanjim/nui.nvim",
     },
-    config = function()
-        require("neo-tree").setup({
-            default_component_configs = {
-                file_size = {
-                    enabled = false,
-                },
-                type = {
-                    enabled = false,
-                },
-                last_modified = {
-                    enabled = false,
-                },
-                created = {
-                    enabled = false,
-                },
+    lazy = false,
+    keys = {
+        -- vim.keymap.set('n', '<C-n>', ':Neotree filesystem toggle reveal float<CR>', {})
+        { '<C-n>', ':Neotree filesystem toggle reveal float<CR>' },
+    },
+    opts = {
+        default_component_configs = {
+            file_size = {
+                enabled = false,
             },
-            filesystem = {
-                filtered_items = {
-                    visisble = true,
-                    hide_dotfiles = false,
-                    hide_gitignored = false,
-                }
+            type = {
+                enabled = false,
             },
-        })
-        vim.keymap.set('n', '<C-n>', ':Neotree filesystem toggle reveal float<CR>', {})
-    end
+            last_modified = {
+                enabled = false,
+            },
+            created = {
+                enabled = false,
+            },
+        },
+        filesystem = {
+            filtered_items = {
+                visisble = true,
+                hide_dotfiles = false,
+                hide_gitignored = false,
+            }
+        },
+    },
 }
