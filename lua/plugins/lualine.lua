95c29b1ff5117ea488c41186da22cd3d97c423a2
Theodore A. Roth <troth@openavr.org>
2026-03-28 06:05:43 -0600

    lualine: Use opts instead of config = function()
    Signed-off-by: Theodore A. Roth <troth@openavr.org>

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,13 +1,11 @@
 return {
     "nvim-lualine/lualine.nvim",
-    config = function()
-        require('lualine').setup({
-            options = {
-                theme = 'Tomorrow'
-            },
-            sections = {
-                lualine_c = { { 'filename', path=1 } }
-            }
-        })
-    end
+    opts = {
+        options = {
+            theme = 'Tomorrow'
+        },
+        sections = {
+            lualine_c = { { 'filename', path=1 } }
+        }
+    }
 }
